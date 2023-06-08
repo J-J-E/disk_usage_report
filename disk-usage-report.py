0b5d7f9959c38a9df09391cf07908d65b4c1a768
J-J-E <John.j.Ehlen@gmail.com>
2023-06-08 13:53:07 -0700

    Converted generate_usagge_file to many different functions
    added cli args to be able to call specific parts of the report, added assertions to prevent conflicting behavior

--- disk-usage-report.py
+++ disk-usage-report.py
@@ -13,6 +13,20 @@ from email.mime.base import MIMEBase
 from email import encoders
 from collections import namedtuple
 
+def format_duration(duration):
+    days = duration.days
+    hours, remainder = divmod(duration.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{days} days, {hours:02d}:{minutes:02d}"
+
+def email_config_check():
+    assert os.environ.get("smtp_server"), "SMTP server not configured. Please set the 'smtp_server' environment variable."
+    assert os.environ.get("smtp_port"), "SMTP port not configured. Please set the 'smtp_port' environment variable."
+    assert os.environ.get("smtp_username"), "SMTP username not configured. Please set the 'smtp_username' environment variable."
+    assert os.environ.get("smtp_password"), "SMTP password not configured. Please set the 'smtp_password' environment variable."
+    assert os.environ.get("email_send_to"), "Email recipient(s) not configured. Please set the 'email_send_to' environment variable."
+    assert os.environ.get("email_send_from"), "Email sender not configured. Please set the 'email_send_from' environment variable."
+
 
 def import_config_values():
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -32,6 +46,7 @@ def disk_partitions(all=False):
     """Return all mounted partitions as a namedtuple.
     If all == False return physical partitions only.
     """
+    disk_ntuple = namedtuple('partition', 'device mountpoint fstype')
     partitions = psutil.disk_partitions(all=all)
     retlist = []
     for part in partitions:
@@ -45,6 +60,7 @@ def disk_partitions(all=False):
 
 def disk_usage(path):
     """Return disk usage associated with the path in gigabytes."""
+    usage_ntuple = namedtuple('usage', 'total used free percent')
     usage = psutil.disk_usage(path)
     total = usage.total / (1024 ** 3)
     used = usage.used / (1024 ** 3)
@@ -58,13 +74,7 @@ def write_disk_usage_info(file_path, contents):
         f.write(contents)
 
 
-def generate_usage_file(device=None, mountpoint=None, all_partitions=False, file_path=None):
-    """Generate a disk usage file for the specified device(s) or mountpoint(s)."""
-    file_contents = ''
-    if file_path is None:
-        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "disk_usage.txt")
-
-    # Operating System information
+def get_os_information():
     uptime = psutil.boot_time()
     uptime_dt = datetime.datetime.fromtimestamp(uptime)
     uptime_str = str(datetime.datetime.now() - uptime_dt)
@@ -75,40 +85,68 @@ def generate_usage_file(device=None, mountpoint=None, all_partitions=False, file
     os_info += f"    Machine: {platform.machine()}\n"
     os_info += f"    Processor: {platform.processor()}\n"
     os_info += f"    Uptime: {uptime_str}\n\n"
-    file_contents += os_info
+    return os_info
 
-    # CPU statistics
+
+def get_cpu_statistics():
     cpu_freq = psutil.cpu_freq()
-
     cpu_percent = psutil.cpu_percent(interval=1)  # CPU usage as a percentage
     logical_cpu_count = psutil.cpu_count(logical=True)  # Number of logical CPUs
-    physical_cpu_count = psutil.cpu_count(logical=False) # Number of physical CPUs
+    physical_cpu_count = psutil.cpu_count(logical=False)  # Number of physical CPUs
     cpu_usage_info = "CPU STATISTICS\n"
     cpu_usage_info += f"    Frequency: {cpu_freq.current:.2f} MHz\n"  # CPU frequency if available
     cpu_usage_info += f"    CPU Usage (at runtime): {cpu_percent:.1f}%\n"
     cpu_usage_info += f"    Number of CPUs (physical): {physical_cpu_count}\n"
     cpu_usage_info += f"    Number of CPUs (logical): {logical_cpu_count}\n"
     cpu_usage_info += f"    Number of Running Processes: {len(psutil.pids())}\n\n"
-    file_contents += cpu_usage_info
+    return cpu_usage_info
 
-    # System Security
+
+def get_system_security(metrics=None):
     security_info = "SYSTEM SECURITY\n"
-    users = psutil.users()
-    security_info += "    Connected User Accounts:\n"
-    for user in users:
-        security_info += f"        Username: {user.name}\n"
-        security_info += f"        Terminal: {user.terminal}\n"
-        security_info += f"        Hostname: {user.host}\n"
-        security_info += f"        Started: {user.started}\n\n"
-    connections = psutil.net_connections()
-    security_info += "    Active Network Connections:\n"
-    for conn in connections:
-        security_info += f"        Local Address: {conn.laddr}\n"
-        security_info += f"        Remote Address: {conn.raddr}\n"
-        security_info += f"        Status: {conn.status}\n\n"
-    file_contents += security_info
+    sub_metrics = []
 
-    # Memory statistics
+    sec_metric = next((metric for metric in metrics.split(',') if 'sec' in metric), None)
+    if sec_metric == "sec":
+        metrics = None
+    else:
+        sub_metrics = [metric for metric in sec_metric.split(':') if metric != "sec"]
+
+    if not metrics or 'cua' in sub_metrics:
+        users = psutil.users()
+        security_info += "    Connected User Accounts:\n"
+        for user in users:
+            started = datetime.datetime.fromtimestamp(user.started)
+            duration = datetime.datetime.now() - started
+            formatted_duration = format_duration(duration)
+            security_info += f"        Username: {user.name}\n"
+            security_info += f"        Terminal: {user.terminal}\n"
+            security_info += f"        Hostname: {user.host}\n"
+            security_info += f"        Session Duration: {formatted_duration}\n\n"
+
+    if not metrics or 'anc' in sub_metrics:
+        connections = psutil.net_connections()
+        security_info += "    Active Network Connections:\n"
+        for conn in connections:
+            security_info += f"        Local Address: {conn.laddr}\n"
+            security_info += f"        Remote Address: {conn.raddr}\n"
+            security_info += f"        Status: {conn.status}\n\n"
+
+    return security_info
+
+
+    if not metrics or 'anc' in sub_metrics:
+        connections = psutil.net_connections()
+        security_info += "    Active Network Connections:\n"
+        for conn in connections:
+            security_info += f"        Local Address: {conn.laddr}\n"
+            security_info += f"        Remote Address: {conn.raddr}\n"
+            security_info += f"        Status: {conn.status}\n\n"
+
+    return security_info
+
+
+def get_memory_statistics():
     memory = psutil.virtual_memory()
     total_memory = memory.total / (1024 * 1024 * 1024)  # Total physical memory in GB
     available_memory = memory.available / (1024 * 1024 * 1024)  # Available memory in GB
@@ -129,11 +167,12 @@ def generate_usage_file(device=None, mountpoint=None, all_partitions=False, file
     memory_usage_info += f"        Total: {swap.total / 1024 / 1024 / 1024:.2f} GB\n"
     memory_usage_info += f"        Used: {swap.used / 1024 / 1024 / 1024:.2f} GB\n"
     memory_usage_info += f"        Free: {swap.free / 1024 / 1024 / 1024:.2f} GB\n\n"
-    file_contents += memory_usage_info
+    return memory_usage_info
 
-    # Disk Statistics
+
+def get_disk_statistics(device=None, mountpoint=None, include_all_partitions=False):
     disk_usage_info = "DISK STATISTICS\n"
-    partitions = disk_partitions(all=all_partitions)
+    partitions = disk_partitions(all=include_all_partitions)
     if mountpoint is None:  # No mount points specified, report all of them
         mount_points = [part.mountpoint for part in partitions]
     else:
@@ -154,8 +193,10 @@ def generate_usage_file(device=None, mountpoint=None, all_partitions=False, file
         disk_usage_info += f"    Free: {usage.free:.2f} GB\n"
         disk_usage_info += f"    Percent Used: {usage.percent:.2f}%\n"
         disk_usage_info += f"    Percent Free: {100 - usage.percent:.2f}%\n\n"
-    file_contents += disk_usage_info
+    return disk_usage_info
 
+
+def get_network_statistics():
     # Hostname
     hostname = socket.gethostname()
     # IP address
@@ -167,7 +208,6 @@ def generate_usage_file(device=None, mountpoint=None, all_partitions=False, file
     host_info = "HOST INFORMATION\n"
     host_info += f"    Hostname: {hostname}\n"
     host_info += f"    IP Address: {ip_address}\n\n"
-    file_contents += host_info
 
     # Format network interfaces
     network_info = "NETWORK INTERFACES\n"
@@ -184,9 +224,11 @@ def generate_usage_file(device=None, mountpoint=None, all_partitions=False, file
             elif addr.family == psutil.AF_LINK:
                 network_info += f"        MAC Address: {addr.address}\n"
         network_info += "\n"
-    file_contents += network_info
 
-    # GPU information
+    return host_info + network_info
+
+
+def get_gpu_information():
     gpus = GPUtil.getGPUs()
     gpu_info = "GPU INFORMATION\n"
     for idx, gpu in enumerate(gpus):
@@ -199,13 +241,36 @@ def generate_usage_file(device=None, mountpoint=None, all_partitions=False, file
         gpu_info += f"            Used: {gpu.memoryUsed/1024:.2f} GB\n"
         gpu_info += f"            Free: {gpu.memoryFree/1024:.2f} GB\n"
         gpu_info += f"        Temperature: {gpu.temperature:.2f} °C\n\n"  # GPU temperature if available
-    file_contents += gpu_info
+    return gpu_info
 
 
+def generate_usage_file(device=None, mountpoint=None, include_all_partitions=False, metrics=None, file_path=None):
+    """Generate a disk usage file for the specified device(s) or mountpoint(s)."""
+    file_contents = ''
+    if file_path is None:
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "disk_usage.txt")
 
+    if metrics is None:
+        metrics = 'os,cpu,sec,mem,dsk,net,gpu'
+    else:
+        metrics = metrics.lower()
 
-    print(file_contents)
-    write_disk_usage_info(file_path, disk_usage_info)
+    if 'os' in metrics:
+        file_contents += get_os_information()
+    if 'cpu' in metrics:
+        file_contents += get_cpu_statistics()
+    if 'sec' in metrics:
+        file_contents += get_system_security(metrics)
+    if 'mem' in metrics:
+        file_contents += get_memory_statistics()
+    if 'dsk' in metrics:
+        file_contents += get_disk_statistics(device=device, mountpoint=mountpoint, include_all_partitions=include_all_partitions)
+    if 'net' in metrics:
+        file_contents += get_network_statistics()
+    if 'gpu' in metrics:
+        file_contents += get_gpu_information()
+
+    write_disk_usage_info(file_path, file_contents)
     return file_path
 
 
@@ -243,17 +308,21 @@ def send_email(send_to, send_from, subject, text, attachment):
         print("Failed to send email:", e)
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--send_email", help="Send email report if set to True", default=False, type=bool)
+    parser.add_argument("--send_email", help="Enabling this flag will send an email based on config settings", action="store_true")
     parser.add_argument("--mount_point", help='Filter disk usage by mount points (comma-separated, enclosed in quotes)', default=None)
+    parser.add_argument("--include_all_partitions", help="Physical partitions only by default. For network/virtual partitions, enable this flag", action="store_true")
+    parser.add_argument("--metrics", help="Sections to include in the report (comma-separated)", default=None)
+
     args = parser.parse_args()
-
     import_config_values()
-    hostname = socket.gethostname()
 
-    disk_ntuple = namedtuple('partition', 'device mountpoint fstype')
-    usage_ntuple = namedtuple('usage', 'total used free percent')
+    # Check Conflicting Arguments
+    if args.mount_point and args.include_all_partitions:
+        raise AssertionError("Conflicting parameters: --mount_point and --include_all_partitions cannot be used together.")
+    if args.send_email:
+        email_config_check()
 
     mount_points = None
     if args.mount_point:
@@ -261,12 +330,17 @@ if __name__ == '__main__':
         # Strip whitespace and remove the enclosing quotes
         mount_points = [mp.strip().strip("'").strip('"') for mp in mount_points]
 
-    file_path = generate_usage_file(mountpoint=mount_points, all_partitions=True)
+    file_path = generate_usage_file(mountpoint=mount_points, include_all_partitions=args.include_all_partitions, metrics=args.metrics)
 
     if args.send_email:
+        hostname = socket.gethostname()
         send_to = os.environ.get("email_send_to")
         send_from = os.environ.get("email_send_from")
         subject = hostname + ' - Disk Usage Report'
         text = 'Please see the attached file.'
         attachment = file_path
         send_email(send_to, send_from, subject, text, attachment)
+
+
+if __name__ == '__main__':
+    main()
